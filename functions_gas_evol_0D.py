added Kataoka function to radmc, ruller to image, corrected column density to compute mean photodissociation lifetime (factor 2), and corrected default values planetesimals strength

functions_gas_evol_0D.py
@@ -19,8 +19,9 @@ G = 6.67408e-11 # mks
 kb = 1.38064852e-23 #mks
 year_s = 3.154e7
 
-sigma_C1c=(1./sigma_c1)*m_c1/Mearth*au_cm**2.0 # mearth/au2
-sigma_COc=(1./sigma_co)*m_co/Mearth*au_cm**2.0 # mearth/au2
+# factor 2 as average column density is ~half of the total one
+sigma_C1c=2*(1./sigma_c1)*m_c1/Mearth*au_cm**2.0 # mearth/au2
+sigma_COc=2*(1./sigma_co)*m_co/Mearth*au_cm**2.0 # mearth/au2
 
 
 ## functions
@@ -87,8 +88,8 @@ def tau_CO(r,dr,MCO, MC1):
 def tau_CO2(Sigma_CO, Sigma_C1):
     #area=2*np.pi*r*dr*au_cm**2.0 # cm2
     
-    NC1=Sigma_C1*Mearth/m_c1/au_cm**2.0
-    NCO=Sigma_CO*Mearth/m_co/au_cm**2.0
+    NC1=Sigma_C1/2.*Mearth/m_c1/au_cm**2.0
+    NCO=Sigma_CO/2.*Mearth/m_co/au_cm**2.0
     
     return 120.0 * np.exp( sigma_c1*NC1)/ selfshielding_CO(NCO) # yr
 

functions_image.py
@@ -491,11 +491,11 @@ def flux_profile(image, image_pb, x0, y0, PA, inc, rmax,Nr, rms,  BMAJ_arcsec, B
             # mask=
             # F[i_r,1]= np.sum( rmsmap2[rdep<rs[i_r]])
 
-    image[mask]=0.0  
-    plt.pcolor(xs, ys, image)
-    plt.contour(xs,ys,rdep, levels=rs[::3])
-    plt.axes().set_aspect('equal')
-    plt.show()
+    # image[mask]=0.0  
+    # plt.pcolor(xs, ys, image)
+    # plt.contour(xs,ys,rdep, levels=rs[::3])
+    # plt.axes().set_aspect('equal')
+    # plt.show()
     return np.array([rs, F[:,0], F[:,1]]) # rs, I, eI
 
 def flux_profile_edgeon(image, image_pb, x0, y0, PA, rmax,Nr, rms, BMAJ_arcsec, BMIN_arcsec, ps_arcsec, rs=np.array([]), refine=1, hv=0.05):
@@ -574,10 +574,10 @@ def flux_profile_edgeon(image, image_pb, x0, y0, PA, rmax,Nr, rms, BMAJ_arcsec, 
         F[i_r,1]= np.sqrt(F[i_r,1]) * np.sqrt(ps_arcsec**2.0/Beam_area)
 
             
-    image[mask]=0.0
-    plt.pcolor(xs, ys, image)
-    plt.axes().set_aspect('equal')
-    plt.show()
+    # image[mask]=0.0
+    # plt.pcolor(xs, ys, image)
+    # plt.axes().set_aspect('equal')
+    # plt.show()
     return np.array([rs, F[:,0], F[:,1]]) # rs, I, eI
 
 def flux_azimuthal_profile(image, image_pb, x0, y0, PA, inc, rmin, rmax, NPA, rms,  BMAJ_arcsec, BMIN_arcsec, ps_arcsec):
@@ -1361,7 +1361,7 @@ def bin_dep_vis(uvmin, uvmax, Nr, us, vs, reals, imags, Inc, PA, weights=[1.0]):
              
     return Rs_edge, Rs, np.array([Real_mean, Real_std, Real_error]), np.array([Imag_mean, Imag_std, Imag_error]), np.array([Amp_mean, Amp_std, Amp_error])
 
-def save_image(filename, image, xedge, yedge, rms=0.0, rmsmap=0.0, vmin=0.0, vmax=100.0, colormap='inferno', tickcolor='white', XMAX=10.0, major_ticks=np.arange(-15, 20.0, 5.0) , minor_ticks=np.arange(-15.0, 15.0+1.0, 1.0), BMAJ=0.0, BMIN=0.0, BPA=0.0, show_beam=True, show=True, clabel=r'Intensity [$\mu$Jy beam$^{-1}$]', formatcb='%1.0f', cbticks=np.arange(-500.0,500.0,50.0), contours=True, star=True, xstar=0.0, ystar=0.0, cbar_log=False, xunit='arcsec', bad_color=(0,0,0)):
+def save_image(filename, image, xedge, yedge, rms=0.0, rmsmap=0.0, vmin=0.0, vmax=100.0, colormap='inferno', tickcolor='white', XMAX=10.0, major_ticks=np.arange(-15, 20.0, 5.0) , minor_ticks=np.arange(-15.0, 15.0+1.0, 1.0), BMAJ=0.0, BMIN=0.0, BPA=0.0, show_beam=True, show=True, clabel=r'Intensity [$\mu$Jy beam$^{-1}$]', formatcb='%1.0f', cbticks=np.arange(-500.0,500.0,50.0), contours=True, star=True, xstar=0.0, ystar=0.0, cbar_log=False, xunit='arcsec', bad_color=(0,0,0), ruller=True, dpc=10.):
 
 
     plt.style.use('style1')
@@ -1443,7 +1443,13 @@ def save_image(filename, image, xedge, yedge, rms=0.0, rmsmap=0.0, vmin=0.0, vma
         # add star
         ax1.plot(xstar , ystar, marker='+', color=tickcolor, markersize=3.5, mew=1.0)
 
-
+    if ruller:
+        x1=-XMAX+2.0*abs(minor_ticks[1]-minor_ticks[0])
+        x2=x1+30./dpc
+        yc=-XMAX+2.0*abs(minor_ticks[1]-minor_ticks[0])
+        plt.plot([x1,x2], [yc, yc], color='white')
+        plt.text(x2, yc*0.95, '30 au', color='white')
+        
     plt.tight_layout()
     plt.subplots_adjust(left=0.17, bottom=0.01, right=0.97, top=1.0)
     print 'saving...'

functions_radmc.py
@@ -1928,14 +1928,28 @@ def dered(lam,f,Av,Rv):  # unredenning according to cardelli's law
 
 
 
-
-
-
-
-
-
-
-
-
-
+# Opacity function from Kataoka+2014
+def f_kappa(a, lam, rho, f , nf, kf):
+    # Kataoka+2014
+    # lam in cm
+    lamc=2.*np.pi*a
+    
+    m=nf(lam*1.0e4)+kf(lam*1.0e4)*1.j
+    eps0=m**2.0
+    F=(eps0-1.)/(eps0+2.)
+    eps_ef=(1.+2.*f*F)/(1.-f*F)
+    mef=cma.sqrt(eps_ef)
+    nef=mef.real
+    kef=mef.imag
+    
+    x=lamc/lam
+    if x<1.0:
+        Qabs=24.*nef*kef*x / ((nef**2.-kef**2. +2.)**2. + (2.*nef*kef)**2.)
+    else: #kef*x<3./8.:
+        Qabs2=8.*kef*x*(nef**3. - (nef**2-1.)**1.5)/(3.*nef)
+        Qabs3=1.-0.1*f
+        Qabs=min(Qabs2, Qabs3)
+    #print x, lam
+    kappa0=3./(4.*a*rho)
+    return kappa0*Qabs
 