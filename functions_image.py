corrected sign problem with k_min k_max when playing with cubes and dv<0

functions_image.py
@@ -1253,14 +1253,12 @@ def moment_0(path_cube, line='CO32', v0=0.0, dvel=10.0,  rmin=0.0, inc=90.0, M_s
    
         return image, xfedge, yfedge, BMAJ, BMIN, BPA, dv, rms_moment0
         
-def moment_0_shifted(cube, xs, ys, ps_arcsec, BMAJ, vs, v0 , x0, y0, PA, inc, M_star, dpc, Dvel0=3.2, f1=1.0, f2=2.3):
-
+def moment_0_shifted(cube, xs, ys, ps_arcsec, BMAJ, vs, v0 , x0, y0, PA, inc, M_star, dpc, Dvel0=3.2, f1=1.0, f2=2.3, sign=1.0):
     Npix=len(cube[0,0,:])
     Nf=len(vs)
-    dv=abs(vs[1]-vs[0])
+    dv=vs[1]-vs[0]
     ### calculate shift matrix
-    shiftm=np.rint(f_shift(Npix, x0, y0, ps_arcsec, PA*np.pi/180., inc*np.pi/180., M_star, dpc, rlim=0.5*BMAJ*dpc)/abs(dv)).astype(int)
-
+    shiftm=int(np.sign(sign))*np.rint(f_shift(Npix, x0, y0, ps_arcsec, PA*np.pi/180., inc*np.pi/180., M_star, dpc, rlim=0.5*BMAJ*dpc)/abs(dv)).astype(int)
     ### calculate moment 0
     moment0=np.zeros((Npix,Npix))
     rmsmap=np.ones((Npix,Npix)) # rms in Moment0
@@ -1270,34 +1268,52 @@ def moment_0_shifted(cube, xs, ys, ps_arcsec, BMAJ, vs, v0 , x0, y0, PA, inc, M_
     Rs=np.sqrt((Xs-x0)**2.+(Ys-y0)**2.)
 
     rlim=BMAJ
-    Dvel1=3.*dv # 6 channels wide
-    k_min1=max(0, int((v0-Dvel1-vs[0])/dv) ) # to use beyond f2*rlim
-    k_max1=min(Nf, int((v0+Dvel1-vs[0])/dv) ) # idem
+    Dvel1=3.*np.abs(dv) # 6 channels wide
+    Dvel0=np.abs(Dvel0) # make sure it is possitive
 
-    k_min0=max(0, int((v0-Dvel0-vs[0])/dv) )
-    k_max0=min(Nf, int((v0+Dvel0-vs[0])/dv) )
- 
+    if dv>0.0:
+        k_min1=max(0, int((v0-Dvel1-vs[0])/dv) ) # to use beyond f2*rlim
+        k_max1=min(Nf, int((v0+Dvel1-vs[0])/dv) ) # idem
+
+        k_min0=max(0, int((v0-Dvel0-vs[0])/dv) )
+        k_max0=min(Nf, int((v0+Dvel0-vs[0])/dv) )
+    else:
+        k_min1=max(0, int((v0+Dvel1-vs[0])/dv) ) # to use beyond f2*rlim
+        k_max1=min(Nf, int((v0-Dvel1-vs[0])/dv) ) # idem
+
+        k_min0=max(0, int((v0+Dvel0-vs[0])/dv) )
+        k_max0=min(Nf, int((v0-Dvel0-vs[0])/dv) )
+        
+        
+    print k_min0, k_max0
     
     for j in xrange(Npix):
         for i in xrange(Npix):
 
-            spectrum_shifted = np.roll(cube[:,j,i], shiftm[j,i], axis=0)
-
+            
             if Rs[j,i]>=rlim*f2: ## safe to use keplerian mask
-                moment0[j,i]=np.sum(spectrum_shifted[k_min1:k_max1+1], axis=0)*dv
+                spectrum_shifted = np.roll(cube[:,j,i], shiftm[j,i], axis=0)
+
+                moment0[j,i]=np.sum(spectrum_shifted[k_min1:k_max1+1], axis=0)*np.abs(dv)
                 
             elif Rs[j,i]>=rlim*f1 and Rs[j,i]<rlim*f2:  ## transition region
+                spectrum_shifted = np.roll(cube[:,j,i], shiftm[j,i], axis=0)
 
                 Dvelx=Dvel0 +(Rs[j,i] - rlim*f1)*(Dvel1-Dvel0)/(f2*rlim-f1*rlim)
 
-                k_minx=max(0, int((v0-Dvelx-vs[0])/dv) )
-                k_maxx=min(Nf, int((v0+Dvelx-vs[0])/dv) )
+                if dv>0.0:
+                    k_minx=max(0, int((v0-Dvelx-vs[0])/dv) )
+                    k_maxx=min(Nf, int((v0+Dvelx-vs[0])/dv) )
+                else:
+                    k_minx=max(0, int((v0+Dvelx-vs[0])/dv) )
+                    k_maxx=min(Nf, int((v0-Dvelx-vs[0])/dv) )
 
-                moment0[j,i]=np.sum(spectrum_shifted[k_minx:k_maxx+1], axis=0)*dv
+                    
+                moment0[j,i]=np.sum(spectrum_shifted[k_minx:k_maxx+1], axis=0)*np.abs(dv)
                 rmsmap[j,i]=np.sqrt((k_maxx-k_minx)*1.0/( (k_max1-k_min1) ))
                
-            else: # too close to star 
-                moment0[j,i]=np.sum(spectrum_shifted[k_min0:k_max0+1], axis=0)*dv
+            else: # too close to star, no shift 
+                moment0[j,i]=np.sum(cube[k_min0:k_max0+1, j, i], axis=0)*np.abs(dv)
                 rmsmap[j,i]=np.sqrt((k_max0-k_min0)*1.0/( (k_max1-k_min1) ))
     
 